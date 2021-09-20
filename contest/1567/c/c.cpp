38eb24773866d672e74fd3eeeab00eb8a268f4f1
booiris <954520770@qq.com>
2021-09-20 23:44:12 +0800

    update 1567 c

--- contest/1567/c/c.cpp
+++ contest/1567/c/c.cpp
@@ -1,23 +1,36 @@
-#include <iostream>
 #include <algorithm>
-#include <cstdio>
 #include <cmath>
+#include <cstdio>
 #include <cstring>
+#include <iostream>
 #include <queue>
-#include <vector>
 #include <set>
+#include <string>
+#include <vector>
 #define INF 0x3f3f3f3f
 using namespace std;
 typedef long long ll;
-int n;
+ll dp[15][2];
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     int t;
-    cin>>t;
-    while(t--)
-    {
-        cin>>n;
+    cin >> t;
+    while (t--) {
+        string n, a, b;
+        cin >> n;
+        int len = n.length();
+        for (int i = 0; i < len; i++) {
+            if (i & 1)
+                b += n[i];
+            else
+                a += n[i];
+        }
+        if (len == 1) {
+            cout << stoi(a) - 1 << "\n";
+        } else {
+            cout << 1LL * (stoi(a) + 1) * (stoi(b) + 1) - 2 << "\n";
+        }
     }
 }
